711cf63453e4c721205d6610713bcbd79e17d3ce
Vural CoLak <vuralcolakcontact@gmail.com>
2022-08-18 16:11:03 +0200

    aufgabe object-exercise

--- solution.js
+++ solution.js
@@ -1,43 +1,97 @@
 //1
+console.log(' ');
+console.log('----Aufgabe-1----');
+console.log(' ');
 const data = { a: 1 };
-//write your code here 
+const isPlainObject = (obj) => {
+    return typeof obj === 'object' && !Array.isArray(obj) && obj !== null
+};
 console.log(isPlainObject(data)); // true
 
 //2
+console.log(' ');
+console.log('----Aufgabe-2----');
+console.log(' ');
+
 const data2 = { a: 1, b: 2 };
-//write your code here 
+
+const makePairs = (obj) => {
+    return Object.entries(obj);
+};
+ 
 console.log(makePairs(data2)); // [['a', 1], ['b', 2]] 
 
 //3
+console.log(' ');
+console.log('----Aufgabe-3----');
+console.log(' ');
+
 const data3 = { a: 1, b: 2 };
-//write your code here 
+
+const without = (obj, key) => {
+    delete obj[key];
+    return obj;
+};
+
 console.log(without(data3, 'b')); // { a: 1 }
 
 
 //4
+console.log(' ');
+console.log('----Aufgabe-4----');
+console.log(' ');
+
 const data4 = { a: 1, b: undefined };
 const data40 = { a:undefined };
-//write your code here 
+
+const isEmpty = (obj) => {
+    return Object.values(obj).every((element) => element === undefined) && Object.values(obj).length !== 0
+}; 
+
 console.log(isEmpty(data4)); // false
 console.log(isEmpty(data40)); // true 
 
 //5
+console.log(' ');
+console.log('----Aufgabe-5----');
+console.log(' ');
+
 const data5 = { a: 1, b: 1 };  
 const data51 = { a: 1, b: 1 };  
 const data52 = { a: 1, b: 2 };
-//write your code here 
+
+const isEqual = (obj1, obj2) => {
+    return JSON.stringify(obj1) === JSON.stringify(obj2);
+};
+
 console.log(isEqual(data5, data51)); // true  
 console.log(isEqual(data5, data52)); // false
 
 //6
-const data6 = { a: { b: [1, 2, 3] } }
-//write your code here 
-console.log(invoke(data6, 'a.b', 'splice', [1, 2])) // [2, 3]
+console.log(' ');
+console.log('----Aufgabe-6----');
+console.log(' ');
+
+// const data6 = { a: { b: [1, 2, 3] } }
+
+// const invoke = (x, y1.y2, splice, [1, 2]) => {
+//     return x.y1.y2.splice(1, 2);
+// };
+
+// console.log(invoke(data6, 'a.b', 'splice', [1, 2])) // [2, 3]
 
 
 //7
+console.log(' ');
+console.log('----Aufgabe-7----');
+console.log(' ');
+
 const data7 = { a: { b: undefined } };
-//write your code here 
+
+const isEmptyDeep = (obj) => {
+    return Object.entries(obj).every((element) => element !== undefined);
+}
+
 console.log(isEmptyDeep(data7));
 
 
